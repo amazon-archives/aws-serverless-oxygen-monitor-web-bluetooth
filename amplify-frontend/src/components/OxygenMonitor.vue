3686fa0292d46b54d557410b9a62fa171a26bd63
Moheeb Zara <moheebz@amazon.com>
2020-08-06 12:23:53 -0700

    add box shadow

--- amplify-frontend/src/components/OxygenMonitor.vue
+++ amplify-frontend/src/components/OxygenMonitor.vue
@@ -211,6 +211,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.userData {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+}
 .button-container {
   padding: 0;
   margin: 0;
